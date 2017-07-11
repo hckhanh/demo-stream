f6bfb464be4f7c40eb8a61f57a1eeb2f130f58b2
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-07-11 23:38:42 +0700

    Add HLS file process when show the video

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ffmpeg = require('fluent-ffmpeg');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,19 +22,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'videos')));
 
 // app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,6 +45,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-require('./stream')
+// require('./stream');
+
+// Generate HLS segments from video file
+setTimeout(() => {
+ffmpeg('./video.mp4')
+  .format('hls')
+  .save('./videos/video.m3u8')
+}, 1000)
 
 module.exports = app;
